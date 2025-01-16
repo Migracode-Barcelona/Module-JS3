571af51a8eb93af9316fc2097a1fd2199e0ba873
Saidumar Akbarov <saidumarakbarov@Saidumars-MacBook-Air.local>
2025-01-16 17:45:34 +0100

    fetch function is created

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -1,2 +1,8 @@
 let fetchedImg = ""
-const image = document.getElementById("image")
+const image = document.getElementById("image");
+
+function fetchData() {
+  return fetch(`https://xkcd.now.sh/?comic=latest`).then(function (data) {
+    return data.json();
+  })
+}
