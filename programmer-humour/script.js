e93bf082bfa3d05e613d1820728b244cc95a8216
Saidumar Akbarov <saidumarakbarov@Saidumars-MacBook-Air.local>
2025-01-16 17:53:55 +0100

    error handling is done

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -2,7 +2,11 @@ let fetchedImg = ""
 const image = document.getElementById("image");
 
 function fetchData() {
-  return fetch(`https://xkcd.now.sh/?comic=latest`).then(function (data) {
+  return fetch(`https://xkcd.now.sh/?comic=latest`)
+  .then(function (data) {
+    if (!data.ok) {
+        throw new Error(`Network error: ${data.status}`);
+    }
     return data.json();
   })
 }
@@ -11,4 +15,7 @@ fetchData().then((obj) => {
 fetchedImg = obj.img
 console.log(fetchedImg, "test2");
 image.setAttribute("src", fetchedImg)
+}).catch((error) => {
+    console.error("Error fetching data:", error);
+    image.setAttribute("alt", "Failed to load image")
 })
