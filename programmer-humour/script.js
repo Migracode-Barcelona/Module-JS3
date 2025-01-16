52eab1d22fc2db5764e567b7e7715a95c4de4a4b
kostiantynkovalchuk <kostiantynkovalchuk32@gmail.com>
2025-01-16 17:57:43 +0100

    error handling added

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -4,6 +4,9 @@ const imgF = document.getElementById("img-f")
 
 function fetchData() {
   return fetch(`https://xkcd.now.sh/?comic=latest`).then(function (data) {
+    if (!data.ok) {
+        throw new Error(`Network error: ${data.status}`);
+    }
     return data.json()
     
   })
@@ -14,5 +17,8 @@ console.log(obj, "test");
 fetchedImg = obj.img
 console.log(fetchedImg, "test2");
 imgF.setAttribute("src", fetchedImg)
-}) 
+}).catch((error) => {
+    console.error("Error fetching data:", error);
+    imgF.setAttribute("alt", "Failed to load image")
+})
 
