24588617177f70d8143fc2c250a005420e7c1777
kostiantynkovalchuk <kostiantynkovalchuk32@gmail.com>
2025-01-16 17:39:12 +0100

    img rendered

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -1,4 +1,6 @@
 let fetchedImg = ""
+const imgF = document.getElementById("img-f")
+
 
 function fetchData() {
   return fetch(`https://xkcd.now.sh/?comic=latest`).then(function (data) {
@@ -11,5 +13,6 @@ fetchData().then((obj) => {
 console.log(obj, "test");
 fetchedImg = obj.img
 console.log(fetchedImg, "test2");
+imgF.setAttribute("src", fetchedImg)
 }) 
 
