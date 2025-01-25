81ef381d022f5f8a7c8bf605714c9ca01ad0fae6
Afere precious <preciousafere@gmail.com>
2025-01-25 10:50:06 +0100

    i debugged my javascript.

--- programmer-humour/script.js
+++ programmer-humour/script.js
@@ -1,17 +1,19 @@
-function comicGenerator(){
+ async function comicGenerator(){
     const comicContainer =document.getElementById("comic-container")
-     
-    fetch(`https://xkcd.now.sh/?comic=latest`).then((response)=>{
-        if (!response.ok) {
+     try{
+   const response =  await fetch(`https://xkcd.now.sh/?comic=latest`)
+    if (!response.ok) {
            throw new Error("failed to get the comic");   
         }
-        return response.json()
-    })
-    .then((data)=>{
-        console.log(data);
+        const resp = await response.json();
+        console.log(resp);
+        comicContainer.innerHTML = 
+        `<img src="${resp.img}" alt="${resp.alt}" />`;
         
-        const image = `<img src="${data.img}" alt="${data.alt}" />`
+    }catch(error){
+        console.log("error")
         comicContainer.innerHTML = `<p style="color: red;">Failed to load the comic. Please try again.</p>`
-    })
+    }
+       
 }
 document.getElementById("fetch-comic").addEventListener("click", comicGenerator);
